bd5dc0cc988a929c24193cc5cda2eb3965c9358e
Clinton Smith <technetronic@airmail.net>
2022-01-28 16:03:10 -0600

    MVC remove test code

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -10,25 +10,7 @@ namespace Web.Controllers
             this.ViewBag.Title = "Home Page";
             return View();
         }
-
-        
-        //[Route("/Error/{statusCode}")]
-        public ActionResult Error() // int statusCode
-        {
-            /*
-            if (statusCode == StatusCodes.Status404NotFound)
-            {
-                return View("~/Views/Error/NotFound.cshtml");
-            }
-            else
-            {
-                return View("~/Views/Error/GeneralError.cshtml");
-            }
-            */
-
-            //return View("~/Views/Error/Error.cshtml");
-            return View();
-        }
+               
 
     }
 }
