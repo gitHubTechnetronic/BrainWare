ba79ac260881a452b7cb7649a4abcaee69a7f79d
Clinton Smith <technetronic@airmail.net>
2022-02-07 16:41:32 -0600

    Added example of dbconfig.js for node_modules folder

---
+++ NodeJS/dbconfig.js
@@ -0,0 +1,10 @@
+const  config = {
+    user:  '', // sql user
+    password:  '', //sql user password
+    server:  'localhost', 
+    database:  'BrainWare',
+    trustServerCertificate: true,
+  }
+  
+  module.exports = config;
+  
